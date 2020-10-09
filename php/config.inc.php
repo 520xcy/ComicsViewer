26c6a3a67389d18cc9eafb65241bb5bb799477d9
TsiayauHsiang <greatcc@vip.qq.com>
2020-10-09 10:36:48 +0800

    兼容php7.2以上

--- php/config.inc.php
+++ php/config.inc.php
@@ -3,12 +3,23 @@
 
 header('Content-Type:text/html;Charset=utf-8');
 define('ROOT_PATH', dirname(__FILE__)); // 网站根目录
-define('DB_DSN', 'sqlite:'.ROOT_PATH.'/../data/xiang');
+define('DB_DSN', 'sqlite:' . ROOT_PATH . '/../data/xiang');
 define('DB_USER', '');
 define('DB_PWD', '');
 // 自动加载文件类
-function __autoload($className) {
-	require_once ROOT_PATH . '/class/'. ucfirst($className) .'.class.php';
+
+if (function_exists('spl_autoload_register')) {
+	//php7.2后使用spl_autoload_register
+	function autoload($className)
+	{
+		require_once ROOT_PATH . '/class/' . ucfirst($className) . '.class.php';
+	}
+	spl_autoload_register('autoload');
+} else {
+	function __autoload($className)
+	{
+		require_once ROOT_PATH . '/class/' . ucfirst($className) . '.class.php';
+	}
 }
 
-require_once ROOT_PATH.'/fun.php';
+require_once ROOT_PATH . '/fun.php';
